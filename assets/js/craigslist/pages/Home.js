e24ee6f0522808d49d631a92892d4cf37f542f81
Matt M Moncada <misakamikotocsr@gmail.com>
2021-01-17 09:54:16 -0700

    constructing the header

---
+++ assets/js/craigslist/pages/Home.js
@@ -0,0 +1,21 @@
+import React, { Component} from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Home extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Joe'
+    }
+  }
+  clickedBtn = () => {
+    console.log('swag')
+  }
+  render () {
+    return (
+      <div>
+        <h1>Connecting People <br>Everywhere :)</h1>
+      </div>
+    )
+  }
+}
